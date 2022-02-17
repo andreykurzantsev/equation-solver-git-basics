c12e813d175df52fea160bb78e3d9c266ea7e0ba
Andrey Kurzantsev <hawkeye54123@gmail.com>
2022-02-17 23:13:35 +0200

    The interactive mode is deconstructed into separate functions. The first full-fledged version of the non-interactive mode has been created.

--- app.cpp
+++ app.cpp
@@ -7,42 +7,19 @@
 using namespace std;
 
 string validNum(char varname);
+void solveEquation(double a, double b, double c);
+void printDoneEquation(double a, double b, double c);
 
 int main(int argc, char *argv[])
 {
+    double a, b, c;
     cout << fixed << showpoint;
     cout << setprecision(3);
     if (argc == 1)
     {
-
-        double a, b, c;
         a = stod(validNum('a'));
         b = stod(validNum('b'));
         c = stod(validNum('c'));
-
-        cout << "Equation is: (" << a << ") x^2 + (" << b << ") x + (" << c << ") = 0" << endl;
-
-        int discriminant = pow(b, 2) - 4 * a * c;
-        double x1;
-        double x2;
-        if (discriminant < 0)
-        {
-            cout << "There are 0 roots" << endl;
-        }
-        else if (discriminant == 0)
-        {
-            x1 = -b / 2 * a;
-            cout << "There are 1 root" << endl;
-            cout << "x = " << x1;
-        }
-        else if (discriminant > 0)
-        {
-            x1 = (-b - sqrt(discriminant)) / (2 * a);
-            x2 = (-b + sqrt(discriminant)) / (2 * a);
-            cout << "There are 2 roots" << endl;
-            cout << "x1 = " << x1 << endl;
-            cout << "x2 = " << x2 << endl;
-        }
     }
     else if (argc == 2)
     {
@@ -57,19 +34,31 @@ int main(int argc, char *argv[])
         else
         {
             getline(inputfile, inlines);
-            strbuf << inlines;
+            strbuf.str(inlines);
 
             inputfile >> checkformat;
             if (checkformat != "")
             {
                 cout << "Invalid file format" << endl;
             }
+            double coefs[3];
+            for (int i = 0; i < 3; i++)
+            {
+                string k;
+                strbuf >> k;
+                coefs[i] = stod(k);
+            }
+            a = coefs[0];
+            b = coefs[1];
+            c = coefs[2];
         }
     }
     else
     {
         cout << "Error" << endl;
     }
+    printDoneEquation(a, b, c);
+    solveEquation(a, b, c);
 
     return 0;
 }
@@ -96,4 +85,34 @@ string validNum(char varname)
         }
     }
     return value;
+}
+
+void solveEquation(double a, double b, double c)
+{
+    double discriminant = pow(b, 2) - 4 * a * c;
+    double x1;
+    double x2;
+    if (discriminant < 0)
+    {
+        cout << "There are 0 roots" << endl;
+    }
+    else if (discriminant == 0)
+    {
+        x1 = -b / 2 * a;
+        cout << "There are 1 root" << endl;
+        cout << "x = " << x1;
+    }
+    else if (discriminant > 0)
+    {
+        x1 = (-b - sqrt(discriminant)) / (2 * a);
+        x2 = (-b + sqrt(discriminant)) / (2 * a);
+        cout << "There are 2 roots" << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+}
+
+void printDoneEquation(double a, double b, double c)
+{
+    cout << "Equation is: (" << a << ") x^2 + (" << b << ") x + (" << c << ") = 0" << endl;
 }
